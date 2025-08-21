372b6bf6bfacf27cc875e7a6835a0173111a04ee
Isaac-1291 <Profkb91@gmail.com>
2025-08-21 10:40:55 +0000

    alx FE week9 project

---
+++ alx-movie-app/pages/_document.tsx
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Link the PWA manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Define a theme color for mobile browsers */}
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
+      <body className="antialiased">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
